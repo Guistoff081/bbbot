0c80af04a10067461adf06a31c03ac65d26f4959
Guistoff081 <55218750+Guistoff081@users.noreply.github.com>
2020-04-04 05:38:18 -0300

    atualizando o codigo

--- src/config/contents.js
+++ src/config/contents.js
@@ -19,8 +19,8 @@ smmmmmdhhddo. \`/hmmmmmds-     \`:.
 
 const links = {
   loginUrl: 'https://minhaconta.globo.com',
-  voteUrl:
-    'https://gshow.globo.com/realities/bbb/bbb20/votacao/paredao-bbb20-quem-voce-quer-eliminar-felipe-manu-ou-mari-a9f49f90-84e2-4c12-a9af-b262e2dd5be4.ghtml',
+  voteUrl:  
+    'https://gshow.globo.com/realities/bbb/bbb20/votacao/paredao-bbb20-quem-voce-quer-eliminar-babu-gabi-ou-thelma-305135b8-b442-4cc8-888f-2a01ed79cc2d.ghtml',
   challengeUrl: 'https://captcha.globo.com/api/challenge/generate',
   challengeAcceptedUrl:
     'https://royale.globo.com/polls/8e81660d-89b5-4777-a324-6f2da878c5c7',
@@ -34,7 +34,7 @@ const XPathContents = {
     '/html/body/div[1]/main/div[2]/div/div/div/div[2]/div[1]/form/div[3]/div[1]/input',
   loginBtn:
     '/html/body/div[1]/main/div[2]/div/div/div/div[2]/div[1]/form/div[6]/button',
-  revoteBtn: '/html/body/div[2]/div[4]/div/div[3]/div/div/div[1]/div[2]/button',
+  revoteBtn: '/html/body/div[1]/div[4]/div/div[3]/div/div/div[1]/div[2]/button',
   captchaImage:
     '/html/body/div[2]/div[4]/div/div[1]/div[4]/div[2]/div[2]/div/div/div[2]/div/div[2]/img'
 };
@@ -45,10 +45,10 @@ const users = [
   '/html/body/div[2]/div[4]/div/div[1]/div[4]/div[3]/div'
 ];
 
-/* 
-  PRIOR = 0
-  MANU = 1 
-  MARI = 2
+/*
+  BABU = 0
+  GABI = 1
+  THELMA = 2
 */
 
 module.exports = {
